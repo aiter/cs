ee1f68b8a8fb562794ffd89b995398e7c9bd71e7
aiter <aiter2006@gmail.com>
2019-06-12 09:56:39 +0000

    reverse list

---
+++ DataStructure/list/reverselinkedlist.c
@@ -0,0 +1,56 @@
+#include<stdio.h> 
+#include<stdlib.h>
+
+struct Node
+{
+	int data;
+	struct Node * next;
+};
+
+static void reverse(struct Node ** head_ref)
+{
+	struct Node* prev = NULL;
+	struct Node* current = *head_ref;
+	struct Node* next = NULL;
+	while(current != NULL) {
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}	
+	*head_ref = prev;
+}
+
+void push(struct Node ** head_ref, int new_data)
+{
+	struct Node * new_node = (struct Node*)malloc(sizeof(struct Node));
+	new_node->data = new_data;
+	new_node->next = (*head_ref);
+	(*head_ref) = new_node;
+}
+
+void printList(struct Node* head) 
+{
+	struct Node *tmp = head;
+	while(tmp != NULL)
+	{
+		printf("%d ", tmp->data);
+		tmp = tmp->next;
+	}
+}
+
+int main(void)
+{
+	struct Node* head = NULL;
+	push(&head, 20); 
+	push(&head, 14); 
+	push(&head, 5);  
+	push(&head, 12); 
+	
+	printf("Given linked list\n"); 
+	printList(head);     
+	reverse(&head);                       
+	printf("\nReversed Linked list \n"); 
+	printList(head);     
+	getchar(); 	
+}

a4b177d6fb5cc22d4788d3f460832ebb0ec23eb6
aiter <aiter2006@gmail.com>
2019-06-13 23:29:28 +0000

    add select()'s comments

--- c-program/socket1/server5.c
+++ c-program/socket1/server5.c
@@ -17,19 +17,19 @@ int main() {
 	struct sockaddr_in client_address;
 	int result;
 	fd_set readfds, testfds;
-	//删除以前的套接字，为服务器创建一个未命名的套接字
-	//unlink("server_socket");
+	//服务器套接字
 	server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	//命名套接字：
 	server_address.sin_family = AF_INET;
 	server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
 	server_address.sin_port = htons(9734);
-	//strcpy(server_address.sun_path, "server_socket");
 	server_len = sizeof(server_address);
+	//bind
 	bind(server_sockfd, (struct sockaddr *)&server_address, server_len);
 	//创建一个连接队列，开始等待客户进行连接：
 	listen(server_sockfd, 5);
 	FD_ZERO(&readfds);
+	//将服务器套接字加入SET
 	FD_SET(server_sockfd, &readfds);
 	while(1) {
 		char ch;
@@ -37,20 +37,21 @@ int main() {
 		int nread;
 		testfds = readfds;
 		printf("server waiting\n");
-		//接受一个连接
-		//client_len = sizeof(client_address);
-		//client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_address, &client_len);
+		//select ,第一次，肯定只有server_sockfd一个fd
 		result = select(FD_SETSIZE, &testfds, (fd_set *)0, (fd_set *)0, (struct timeval *) 0);
 		if(result < 1) {
 			perror("server5");
 			exit(1);
 		}
+		//select到可读、可写的fd，就遍历SET
+		// epoll最主要的区别在这里？select需要从0遍历所有的fd，然后检查是否属于SET中的
 		for(fd = 0; fd < FD_SETSIZE; fd++) {
 			if(FD_ISSET(fd, &testfds)) { 
 				//如果是server_sockfd上的，那么是新的链接
 				if(fd == server_sockfd) {
 					client_len = sizeof(client_address);
 					client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_address, &client_len);
+					//添加新的fd，“下次”select就可能有这个fd了
 					FD_SET(client_sockfd, &readfds);
 					printf("adding client on fd %d\n", client_sockfd);
 				}else {

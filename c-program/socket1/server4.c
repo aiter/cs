92b965be10492411c062a391dd7d095fec160d90
aiter <aiter2006@gmail.com>
2019-06-13 23:32:30 +0000

    add comment for socket(fork() child-proccess)

--- c-program/socket1/server4.c
+++ c-program/socket1/server4.c
@@ -11,20 +11,16 @@
 int main() {
 	int server_sockfd, client_sockfd;
 	int server_len, client_len;
-	//struct sockaddr_un server_address;
-	//struct sockaddr_un client_address;
 	struct sockaddr_in server_address;
 	struct sockaddr_in client_address;
-	//删除以前的套接字，为服务器创建一个未命名的套接字
-	//unlink("server_socket");
+	//server
 	server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	//命名套接字：
 	server_address.sin_family = AF_INET;
 	server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
 	server_address.sin_port = htons(9734);
-	//server_address.sin_port = 9734;
-	//strcpy(server_address.sun_path, "server_socket");
 	server_len = sizeof(server_address);
+	//bind
 	bind(server_sockfd, (struct sockaddr *)&server_address, server_len);
 	//创建一个连接队列，开始等待客户进行连接：
 	listen(server_sockfd, 5);
@@ -36,6 +32,7 @@ int main() {
 		client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_address, &client_len);
 		//对client_sockfd套接字上的客户进行读写操作
 		if(fork() == 0) {//fork
+			//使用fork出的子进程来处理
 			read(client_sockfd, &ch, 1);
 			sleep(5);
 			ch++;

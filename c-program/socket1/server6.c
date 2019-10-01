a17ab3a07d0fc306cf2f2c71e45d64eda3c688c1
aiter <aiter2006@gmail.com>
2019-10-01 00:15:04 +0000

    nonblocking

--- c-program/socket1/server6.c
+++ c-program/socket1/server6.c
@@ -49,12 +49,113 @@ int onSocketRead(int fd, struct Buffer *buffer) {
 			}
 		}
 	}
+
+	if (result == 0) {
+		return 1;
+	} else if (result < 0) {
+		if (error == EAGAIN)
+			return 0;
+		return -1;
+	}
 }
 
-if (result == 0) {
-	return 1;
-} else if (result < 0) {
-	if (error == EAGAIN)
-		return 0;
-	return -1;
+int onSocketWrite(int fd, struct Buffer *buffer) {
+	while (buffer->readable < buffer->writeIndex) {
+		ssize_t result = send(fd, buffer->buffer + buffer->readIndex, buffer->writeIndex - buffer->readIndex, 0);
+		if (result < 0) {
+			if (errno == EAGAIN)
+				return 0;
+			return -1;
+		}
+
+		buffer->readIndex += result;
+	}
+
+	if (buffer->readIndex == buffer->writeIndex)
+		buffer->readIndex = buffer->writeIndex = 0;
+
+	buffer->readable = 0;
+
+	return 0;
 }
+
+int main(int argc, char **arvg) {
+	int listen_fd;
+	int i, maxfd;
+
+	struct Buffer *buffer[FD_INIT_SIZE];
+	for (i = 0; i < FD_INIT_SIZE; ++i) {
+		buffer[i] = alloc_buffer();
+	}
+
+	listen_fd = tcp_nonblocking_server_listen(SERV_PORT);
+
+	fd_set readset, writeset, exset;
+	FD_ZERO(&readset);
+	FD_ZERO(&writeset);
+	FD_ZERO(&exset);
+	
+	while (1) {
+		maxfd = listen_fd;
+
+		FD_ZERO(&readset);
+		FD_ZERO(&writeset);
+		FD_ZERO(&exset);
+
+		FD_SET(listen_fd, &readset);
+
+		for (i = 0; i < FD_INIT_SIZE; ++i) {
+			if (buffer[i]->connect_fd > 0) {
+				if (buffer[i]->connect_fd > maxfd)
+					maxfd = buffer[i]->connect_fd;
+				FD_SET(buffer[i]->connect_fd, &readset);
+				if (buffer[i]->readable) {
+					FD_SET(buffer[i]->connect_fd, &writeset);
+				}
+			}
+		}
+
+		if (select(maxfd + 1, &readset, &writeset, &exset, NULL) < 0) {
+			error(1, errno, "select error");
+		}
+
+		if (FD_ISSET(listen_fd, &readset)) {
+			printf("listening socket readable\n");
+			sleep(5);
+			struct  sockaddr_storage ss;
+			socklen_t slen = sizeof(ss);
+			int fd = accept(listen_fd, (struct sockaddr *) &ss, &slen);
+			if (fd < 0) {
+				error(11, errno, "accept failed");
+			}else if (fd > FD_INIT_SIZE) {
+				error(1, 0 , "too many connections");
+				close(fd);
+			}else {
+				make_nonblocking(fd);
+				if (buffer[fd]->connect_fd == 0) {
+					buffer[fd]->connect_fd = fd;
+				} else {
+					error(1, 0, "too many connections");
+				}
+			}
+		}
+
+		for (i = 0; i < maxfd + 1; ++i) {
+			int r = 0;
+			if (i == listen_fd)
+				continue;
+
+			if (FD_ISSET(i, &readset)) {
+				r = onSocketRead(i, buffer[i]);
+			}
+			if (r == 0 && FD_ISSET(i, &writeset)) {
+				r = onSocketWrite(i, buffer[i]);
+			}
+			if (r) {
+				buffer[i]->connect_fd = 0;
+				close(i);
+			}
+		}
+	}
+}
+

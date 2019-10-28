657a340503c0ba00f39745a93319a03290bbeac5
aiter <aiter2006@gmail.com>
2019-10-28 11:15:35 +0000

    add log

---
+++ c-program/socket1/lib/common.h
@@ -0,0 +1,16 @@
+#ifndef SOCKET_COMMON_H
+#define SOCKET_COMMON_H
+
+#include "config.h"
+
+#include "log.h"
+
+
+
+#define SERV_PORT      43211
+#define MAXLINE        4096
+#define UNIXSTR_PATH   "/var/lib/unixstream.sock"
+#define LISTENQ        1024
+#define BUFFER_SIZE    4096
+
+#endif //SOCKET_COMMON_H

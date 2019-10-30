3cd67483eb37d107549b476b8789b6935d742102
aiter <aiter2006@gmail.com>
2019-10-30 11:20:26 +0000

    add log

--- c-program/socket1/lib/common.h
+++ c-program/socket1/lib/common.h
@@ -1,10 +1,37 @@
 #ifndef SOCKET_COMMON_H
 #define SOCKET_COMMON_H
 
-#include "config.h"
+//#include "config.h"
 
 #include "log.h"
 
+#include    <sys/types.h>    /* basic system data types */
+#include    <sys/socket.h>    /* basic socket definitions */
+#include    <sys/time.h>    /* timeval{} for select() */
+#include    <time.h>        /* timespec{} for pselect() */
+#include    <netinet/in.h>    /* sockaddr_in{} and other Internet defns */
+#include    <arpa/inet.h>    /* inet(3) functions */
+#include    <errno.h>
+#include    <fcntl.h>        /* for nonblocking */
+#include    <netdb.h>
+#include    <signal.h>
+#include    <stdio.h>
+#include    <stdlib.h>
+#include    <string.h>
+#include    <sys/stat.h>    /* for S_xxx file mode constants */
+#include    <sys/uio.h>        /* for iovec{} and readv/writev */
+#include    <unistd.h>
+#include    <sys/wait.h>
+#include    <sys/un.h>        /* for Unix domain sockets */
+
+#include    <sys/select.h>    /* for convenience */
+#include    <sys/sysctl.h>
+#include    <poll.h>        /* for convenience */
+#include    <strings.h>        /* for convenience */
+#include    <sys/ioctl.h>
+#include    <pthread.h>
+
+void err_dump(const char *, ...);
 
 
 #define SERV_PORT      43211

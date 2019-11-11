4d3d7886088da289670818012b30b693d46f835a
aiter <aiter2006@gmail.com>
2019-11-11 00:17:08 +0000

    tcp server

--- c-program/socket1/lib/tcp_server.c
+++ c-program/socket1/lib/tcp_server.c
@@ -118,4 +118,47 @@ struct TCPserver * tcp_server_init(struct event_loop *eventLoop, struct acceptor
 	return tcpServer;
 }
 
+int handle_connection_established(void *data) {
+	struct TCPserver *tcpServer = (struct TCPserver *) data;
+	struct acceptor *acceptor = tcpServer->acceptor;
+	int listenfd = acceptor->listen_fd;
 
+	struct sockaddr_in client_addr;
+	socklen_t client_len = sizeof(client_addr);
+	int connected_fd = accept(listenfd, (struct sockaddr *)&client_addr, &client_len);
+	make_nonblocking(connected_fd);
+
+	yolanda_msgx("new connection established, socket == %d", connected_fd);
+
+	// choosae event loop from the thread pool
+	struct event_loop *eventLoop = thread_pool_get_loop(tcpServer->threadPool);
+
+	//create a new tcp connection
+	struct tcp_connection *tcpConnection = tcp_connection_new(connected_fd, eventLoop, tcpServer->connectionCompletedCallBack, tcpServer->connectionClosedCallBack, tcpServer->messageCallBack, tcpServer->writeCompletedCallBack);
+	//for callback use
+	if (tcpServer->data != NULL) {
+		tcpConnection->data = tcpServer->data;
+	}
+	return 0;
+}
+
+//开启监听
+void tcp_server_start(struct TCPserver *tcpServer) {
+	struct acceptor *acceptor = tcpServer->acceptor;
+	struct event_loop *eventLoop = tcpServer->eventLoop;
+
+	//开启多个线程
+	thread_pool_start(tcpServer->threadPool);
+
+	//acceptor主线程，同时把tcpServer作为参数传递给channel对象
+	struct channel *channel = channel_new(acceptor->listen_fd, EVENT_READ, handle_connection_established, NULL, tcpServer);
+	event_loop_add_channel_event(eventLoop, channel->fd, channel);
+	return;
+}
+
+//设置callback数据
+void tcp_server_set_data(struct TCPserver *tcpServer, void *data) {
+	if (data != NULL) {
+		tcpServer->data = data;
+	}
+}

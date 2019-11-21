1a9e457e0388c3fb25386bdb52da7c67fb286ec6
aiter <aiter2006@gmail.com>
2019-11-21 20:23:10 +0800

    update read

--- c-program/socket1/lib/read.c
+++ c-program/socket1/lib/read.c
@@ -74,3 +74,52 @@ size_t readline(int fd, char *buffer, size_t length) {
 	return -1;
 }
 
+size_t read_message(int fd, char *buffer, size_t length) {
+	u_int32_t msg_length;
+	u_int32_t msg_type;
+	int rc;
+
+	rc = readn(fd, (char *) &msg_length, sizeof(u_int32_t));
+	if (rc != sizeof(u_int32_t))
+		return rc < 0 ? -1 : 0;
+	msg_length = ntohl(msg_length);
+
+	rc = readn(fd, (char *) &msg_type, sizeof(msg_type));
+	if (rc != sizeof(u_int32_t))
+		return rc < 0 ? -1 : 0;
+
+	if (msg_length > length) {
+		return -1;
+	}
+
+	rc = readn(fd, buffer, msg_length);
+	if (rc != msg_length)
+		return rc < 0 ? -1 : 0;
+	return rc;
+}
+
+int read_line(int fd, char *buf, int size) {
+	int i = 0;
+	char c = '\0';
+	int n;
+
+	while ((i < size - 1) && (c != '\n')) {
+		n = recv(fd, &c, 1, 0);
+		if (n > 0) {
+			if (c == '\r') {
+				n = recv(fd, &c, 1, MSG_PEEK);
+				if ((n > 0) && (c == '\n'))
+					recv(fd, &c, 1, 0);
+				else 
+					c = '\n';
+			}
+			buf[i] = c;
+			i++;
+		} else {
+			c = '\n';
+		}
+	}
+	buf[i] = '\0';
+
+	return i;
+}

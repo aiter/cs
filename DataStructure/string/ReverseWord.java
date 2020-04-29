877f5faf53c123487bf5515cb4d55d446b9fcfbf
aiter <aiter2006@gmail.com>
2020-04-29 20:30:25 +0800

    reverse word

---
+++ DataStructure/string/ReverseWord.java
@@ -0,0 +1,40 @@
+public class ReverseWord{
+    public static void main(String[] args){
+        String str="hello    World java14";
+        char[] chars=str.toCharArray();
+        int left=0;
+        int right=chars.length-1;
+        swapDic(chars,left,right);
+        left=0;
+        right=0;
+        for(int i=0;i<chars.length;i++){
+            if(chars[i]==' '){
+                swapDic(chars,left,right-1);
+                left=right+1;
+            }else if(i==chars.length-1){
+                swapDic(chars,left,right);
+            }
+            right++;
+        }
+        System.out.println(chars);
+    }
+
+    /*翻转字符串*/
+    public static void swapDic(char[] chars,int left,int right){
+        while(left<right){
+            swap(chars,left,right);
+            left++;
+            right--;
+        }
+    }
+
+    /*字母对调*/
+    public static void swap(char[] chars,int n,int m){
+        if(n==m){
+            return;
+        }
+        char temp=chars[n];
+        chars[n]=chars[m];
+        chars[m]=temp;
+    }
+}

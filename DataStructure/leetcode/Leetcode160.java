7ad44f0142fdf691bca6bf2ef47f57e6ab953cd1
aiter <aiter2006@gmail.com>
2020-05-04 21:12:04 +0800

    comprehension

--- DataStructure/leetcode/Leetcode160.java
+++ DataStructure/leetcode/Leetcode160.java
@@ -1,5 +1,7 @@
 /**
  * Write a program to find the node at which the intersection of two singly linked lists begins.
+ * 
+ * 实际的运用场景是什么？ 主要是思路
  *
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/intersection-of-two-linked-lists
@@ -43,6 +45,10 @@ public class Leetcode160 {
 		}
 	}
 
+	/**
+	 *  定义两个指针, 第一轮让两个到达末尾的节点指向另一个链表的头部, 最后如果相遇则为交点(在第一轮移动中恰好抹除了长度差)
+	 *  两个指针等于移动了相同的距离, 有交点就返回, 无交点就是各走了两条指针的长度
+	 **/
 	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
 		if(headA == null || headB == null) {
 			return null;
@@ -51,6 +57,7 @@ public class Leetcode160 {
 		ListNode first = headA;
 		ListNode second = headB;
 
+		// 在这里第一轮体现在pA和pB第一次到达尾部会移向另一链表的表头, 而第二轮体现在如果pA或pB相交就返回交点, 不相交最后就是null==null
 		while(first != second) {
 			first = first == null? headB : first.next;
 			second = second == null? headA : second.next;

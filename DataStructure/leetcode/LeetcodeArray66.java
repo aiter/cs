c692e771e706bf5cdb0d4a43b1551fbd87dc9c95
aiter <aiter2006@gmail.com>
2020-05-22 19:56:28 +0800

    add solution

--- DataStructure/leetcode/LeetcodeArray66.java
+++ DataStructure/leetcode/LeetcodeArray66.java
@@ -3,38 +3,43 @@ import java.util.Arrays;
  * 
  * https://leetcode-cn.com/problems/plus-one/
  *
+ * Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+ *
+ * The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+ *
+ * You may assume the integer does not contain any leading zero, except the number 0 itself.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/plus-one
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class LeetcodeArray66 {
 
-	public static int maxSubArray(int[] nums) {
-		if(nums.length<=0) {
-			return 0;
+	public static int[] plusOne(int[] digits) {
+		int len = digits.length;
+		// 加速处理最后一位非9的数组
+		if(digits[len-1] != 9) {
+			digits[len-1] ++ ;
+			return digits;
 		}
-		//dp[i]：以数组nums[i]为结尾的最大数组和。
-		int[] dp = new int[nums.length];
-		dp[0] = nums[0];
-		int res = dp[0];
-		for(int i=1;i<nums.length;i++){
-			dp[i] = max(nums[i],dp[i-1]+nums[i]);
-			res = max(res,dp[i]);
-		}
-		return res;	
-    }
-
-	public int[] plusOne(int[] digits) {
-
-	}
-
-	public static int max(int a, int b) {
-		return (a >= b) ? a : b;
+		//逻辑想清楚
+		for(int i = len -1 ;i >=0;i--) {
+			if(digits[i]!=9){
+				digits[i]++;
+				return digits;
+			}
+			digits[i]=0;
+		}	
+		int [] news = new int[len+1];
+		news[0]=1;
+		return news;
 	}
 
 	public static void main(String [] args) {
-		int[][] nums = new int[][]{{1,-3,5,6},{1,3,-5,6},{-1,3,5,6},{1,3,5,-6}};
+		int[][] nums = new int[][]{{8,9,9,9},{1,3,5,6},{1,3,6},{9,9,8},{9,9,9}};
 		for(int j = 0 ; j < nums.length ;j++){
-			int len = maxSubArray(nums[j]);
-			System.out.println("len="+len);
-			System.out.println(Arrays.toString(nums[j]));
+			int []news = plusOne(nums[j]);
+			System.out.println(Arrays.toString(news));
 		}
 	}
 }

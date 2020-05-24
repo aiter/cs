9f7d919b2fdb1156551d25e5196eef14696c9b71
aiter <aiter2006@gmail.com>
2020-05-24 22:09:59 +0800

    memory space O(1)

--- DataStructure/leetcode/LeetcodeArray88.java
+++ DataStructure/leetcode/LeetcodeArray88.java
@@ -3,7 +3,29 @@ import java.util.Arrays;
  * 
  */
 public class LeetcodeArray88 {
+	/**
+	 * 空间复杂度更小，不用新的空间，相比merge_0
+	 */
 	public static void merge(int[] nums1, int m, int[] nums2, int n) {
+		int idx = m + n - 1;
+		// idx > m 如果只有nums1了，不用移动了
+		m--;
+		n--;
+		while(idx >= 0 && idx > m) {
+			//只有nums2中有剩余了，直接移动到num1前面
+			if(m<0) {
+				nums1[idx--] = nums2[n--];
+				continue;
+			}
+			if(nums2[n] >= nums1[m]) {
+				nums1[idx--] = nums2[n--];
+			}else {
+				nums1[idx--] = nums1[m--];
+			}	
+		}
+	}
+	
+	public static void merge_0(int[] nums1, int m, int[] nums2, int n) {
 		//数组nums的复制
 		int [] news = new int[m];
 		for(int i = 0;i < m ;i++) {
